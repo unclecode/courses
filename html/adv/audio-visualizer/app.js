2ed7259835728904b66b87eb95640d731be2001f
UncleCode (Hossein Tohidi) <unclecode@kidocode.com>
2019-03-21 00:06:05 +0800

    Create app.js

---
+++ html/adv/audio-visualizer/app.js
@@ -0,0 +1,66 @@
+KStudio
+My Files
+Homeworks
+Messages
+ 
+webT01001.1.1.1.advHtmlAudioVisualizer.html
+JS
+CSS
+      
+ * licenced under the MIT license
+
+
+2930313233343536373839404142434445464748495051525354555657585960616263646566676869707172
+    // frequencyBinCount tells you how many values you'll receive 
+        from the analyser
+    var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+    // we're ready to receive some data!
+    var canvas = document.getElementById('canvas'),
+        cwidth = canvas.width,
+        cheight = canvas.height - 2,
+        meterWidth = 10, //width of the meters in the spectrum
+        gap = 2, //gap between meters
+        capHeight = 2,
+        capStyle = '#fff',
+        meterNum = 800 / (10 + 2), //count of the meters
+        capYPositionArray = []; ////store the vertical position of 
+            hte caps for the preivous frame
+    ctx = canvas.getContext('2d'),
+    gradient = ctx.createLinearGradient(0, 0, 0, 300);
+    gradient.addColorStop(1, '#0f0');
+    gradient.addColorStop(0.5, '#ff0');
+    gradient.addColorStop(0, '#f00');
+    // loop
+    function renderFrame() {
+        var array = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(array);
+        var step = Math.round(array.length / meterNum); //sample 
+            limited data from the total array
+        ctx.clearRect(0, 0, cwidth, cheight);
+        for (var i = 0; i < meterNum; i++) {
+            var value = array[i * step];
+            if (capYPositionArray.length < Math.round(meterNum)) {
+                capYPositionArray.push(value);
+            };
+            ctx.fillStyle = capStyle;
+            //draw the cap, with transition effect
+            if (value < capYPositionArray[i]) {
+                ctx.fillRect(i * 12, cheight - 
+                    (--capYPositionArray[i]), meterWidth, capHeight
+                    );
+            } else {
+                ctx.fillRect(i * 12, cheight - value, meterWidth, 
+                    capHeight);
+                capYPositionArray[i] = value;
+            };
+            ctx.fillStyle = gradient; //set the filllStyle to 
+                gradient for a better look
+            ctx.fillRect(i * 12 /*meterWidth+gap*/ , cheight - value 
+                + capHeight, meterWidth, cheight); //the meter
+        }
+        requestAnimationFrame(renderFrame);
+    }
+    renderFrame();
+App View preview   
+
+
